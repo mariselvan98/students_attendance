d33411986d602fd37eb9ef731dd17fed36cee18e
Mari Selvan <61959655+mariselvan98@users.noreply.github.com>
2022-09-08 01:32:41 +0530

    Update response.dart

--- lib/utils/response.dart
+++ lib/utils/response.dart
@@ -3,73 +3,28 @@ class Response {
   List attendanceData = [
     {
       "attendanceDate": "2022-09-01",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 2, "reason": "fever"}
-      ]
+      "forenoon": {"session": 1, "status": 1, "reason": ""},
+      "afternoon": {"session": 2, "status": 2, "reason": "fever"}
     },
     {
       "attendanceDate": "2022-09-02",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 1, "reason": ""}
-      ]
+      "forenoon": {"session": 1, "status": 1, "reason": ""},
+      "afternoon": {"session": 2, "status": 2, "reason": "fever"}
     },
     {
       "attendanceDate": "2022-09-03",
-      "attendanceDetails": [
-        {"session": 1, "status": 2, "reason": ""},
-        {"session": 2, "status": 2, "reason": "festival"}
-      ]
+      "forenoon": {"session": 1, "status": 1, "reason": ""},
+      "afternoon": {"session": 2, "status": 1, "reason": "fever"}
     },
     {
       "attendanceDate": "2022-09-04",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 1, "reason": ""}
-      ]
+      "forenoon": {"session": 1, "status": 1, "reason": ""},
+      "afternoon": {"session": 2, "status": 2, "reason": "fever"}
     },
     {
       "attendanceDate": "2022-09-05",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 1, "reason": ""}
-      ]
+      "forenoon": {"session": 1, "status": 2, "reason": ""},
+      "afternoon": {"session": 2, "status": 2, "reason": "fever"}
     },
-    {
-      "attendanceDate": "2022-09-06",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 2, "reason": "fever"}
-      ]
-    },
-    {
-      "attendanceDate": "2022-09-07",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 1, "reason": ""}
-      ]
-    },
-    {
-      "attendanceDate": "2022-09-08",
-      "attendanceDetails": [
-        {"session": 1, "status": 2, "reason": ""},
-        {"session": 2, "status": 2, "reason": "festival"}
-      ]
-    },
-    {
-      "attendanceDate": "2022-09-09",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 1, "reason": ""}
-      ]
-    },
-    {
-      "attendanceDate": "2022-09-10",
-      "attendanceDetails": [
-        {"session": 1, "status": 1, "reason": ""},
-        {"session": 2, "status": 1, "reason": ""}
-      ]
-    }
   ];
 }
